3a43b9aade9fa57b1f075bca1c4be213fcaaf14d
softwarecomet <leapord198414@outlook.com>
2020-11-21 01:27:00 +0800

    update

--- src/Views/PhotoLiveness/PhotoLiveness.js
+++ src/Views/PhotoLiveness/PhotoLiveness.js
@@ -5,6 +5,7 @@ import captureImg from "../../assets/camera_take.png"
 import Webcam from '../../Components/Webcam.react';
 import UndetectImgURL from "../../assets/ic_undetected.png"
 import DetectImgURL from "../../assets/ic_detected.png"
+import LogoURL from "../../assets/ic_logo1.png"
 import Button from "../../Components/button/button"
 import { captureUserMedia, VideoUpload, changeCamera, durationFormat } from '../../lib/BackUtils';
 import { PhotoUpload } from '../../lib/AppUtils';
@@ -23,7 +24,8 @@ class PhotoLiveness extends Component {
             captureImgSrc: captureImg,
             ImageURL: null,
             ImgSrc: UndetectImgURL,
-            apiFlage:false
+            logoSrc:LogoURL,
+            apiFlage: false
 
         };
         this.requestUserMedia = this.requestUserMedia.bind(this);
@@ -66,13 +68,13 @@ class PhotoLiveness extends Component {
         var height = this.webcamRef.current.videoRef.current.videoHeight * (this.captureRef.current.width / this.webcamRef.current.videoRef.current.videoWidth);
         context.drawImage(this.webcamRef.current.videoRef.current, 0, 0, this.captureRef.current.width, height);
         var data = this.captureRef.current.toDataURL('image/jpeg');
-        
-        this.setState({apiFlage: true})
+
+        this.setState({ apiFlage: true })
         PhotoUpload(data, (total, progress) => {
         }).then(res => {
-            this.setState({apiFlage: false})
+            this.setState({ apiFlage: false })
             var response = res.data;
-            
+
             if (response.result === "LIVENESS") {
                 alert(response.result + response.score)
 
@@ -85,7 +87,7 @@ class PhotoLiveness extends Component {
 
         }).catch(e => {
             alert("the server is not working, Please try again.");
-            this.setState({apiFlage: false})
+            this.setState({ apiFlage: false })
         })
 
     }
@@ -104,14 +106,16 @@ class PhotoLiveness extends Component {
                     <p style={{ font: "18px", color: "white", textAlign: "center" }}>Please place your face on the oval and take the photo </p>
                     <img src={this.state.captureImgSrc} className="captureIcon" />
                 </div>
-                {(this.state.apiFlage) && <div style={{width:"100%",height:window.innerHeight,zIndex:20,background:"#7f00ff",position:"absolute"}}></div>}
-                {(this.state.apiFlage) &&<div className = "loadingView">
+                <div style={{ width: "100%", height: window.innerHeight, zIndex: 20, background: "#7f00ff", position: "absolute",textAlign:"center" }}>
+                    <img src={this.state.logoSrc} style = {{width:"100px",marginTop:window.innerHeight - 100}}/>
+                </div>
+                {(this.state.apiFlage) && <div className="loadingView">
                     <Loader
                         type="Puff"
                         color="#00BFFF"
                         height={100}
                         width={100}
-                        // timeout={3000} //3 secs
+                    // timeout={3000} //3 secs
                     />
                 </div>}
 
