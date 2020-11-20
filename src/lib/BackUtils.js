a3458b9d33dd2886cf7d273d39528a786f4cbcb8
softwarecomet <leapord198414@outlook.com>
2020-11-20 17:02:00 +0800

    update

--- src/lib/BackUtils.js
+++ src/lib/BackUtils.js
@@ -4,7 +4,7 @@ import datauritoblob from 'datauritoblob'
 export function captureUserMedia(callback, deviceId, facingMode) {  
     var params = {
       audio: false, video: {
-        deviceId: deviceId ? { deviceId: { exact: deviceId } } : null,
+        // deviceId: deviceId ? { deviceId: { exact: deviceId } } : null,
         width: { exact: 1280 },
         height: { exact: 720 },
         // facingMode: facingMode,
