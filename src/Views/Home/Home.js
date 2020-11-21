b53ce3227a0d14ee426f30e28198946c17b38a66
softwarecomet <leapord198414@outlook.com>
2020-11-21 14:08:31 +0800

    update home

--- src/Views/Home/Home.js
+++ src/Views/Home/Home.js
@@ -27,19 +27,67 @@ class Home extends Component {
         return (
             <div>
                 <div className="body-container">
-                    <div className="logoView" style={{ height: window.innerHeight * 0.15, marginBottom: window.innerHeight * 0.2, marginTop: window.innerHeight * 0.09 }}>
+                    <div className="logoView" style={{ height: window.innerHeight * 0.12, marginBottom: "8px", marginTop: window.innerHeight * 0.01 }}>
                         <img src={this.state.ImageSrcs} className="logoIcon" />
+                    </div>
+                    <div className="header">
+                        <div className="companyName">Company Name</div>
+                        <div className="companyDesc">(My Organization short name or slogan)</div>
+                    </div>
+                    <div className="content">
+                        <div className="info">
+                            <p className='heading'>VERIFY YOUR IDENTITY</p>
+                            <p className="desc">To verify your identity, you will use your mobile device de capture following persional data:</p>
+                        </div>
+                        <div className="personal_data_list">
+                            <div className="personal_data">
+                                <div className="title_panel">
+                                    <div className="number">1</div>
+                                    <div className="title"><p>Identity Document <span>(Choose One)</span></p></div>                                    
+                                </div>
+                                <div className="data">
+                                    <ul>
+                                        <li>Passport</li>
+                                        <li>National ID Card</li>
+                                        <li>Residence Permit</li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div className="personal_data">
+                                <div className="title_panel">
+                                    <div className="number">2</div>
+                                    <div className="title"><p>Proof of Address <span>(Choose One)</span></p></div>                                    
+                                </div>
+                                <div className="data">
+                                    <ul>
+                                        <li>Electrical or Water bill document</li>
+                                        <li>Gaz bill document</li>
+                                        <li>Phone bill document</li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div className="personal_data">
+                                <div className="title_panel">
+                                    <div className="number">3</div>
+                                    <div className="title"><p>Self Photo</p></div>                                    
+                                </div>
+                            </div>
+                        </div>
+                        <div class='termsOfService'>
+                            By click on <strong>Start</strong> button you agree to our <a href="#">Terms of Service</a>. For more information see our <a href="#">Privacy Policy</a>.
+                        </div>
                     </div>
                     <Button
                         label="Start"
                         onClick={() => {
-                            if (this.state.flag)
-                                this.props.history.push('idmain');
-                            else
-                                alert('Please the General Condition and Policy')
+                            this.props.history.push('idmain');
+                            // if (this.state.flag)
+                            // this.props.history.push('idmain');
+                            // else
+                            //     alert('Please the General Condition and Policy')
                         }}
                     />
-                    <div style = {{marginTop: "50px"}}>
+                    {/* <div style = {{marginTop: "10px"}}>
                         <Checkbox
                             icon={<img src={this.state.checkSrc} style={{ width: 14 }} />}
                             name="my-input"
@@ -55,7 +103,7 @@ class Home extends Component {
                             labelStyle={{ marginLeft: 5, userSelect: "none", borderColor: "white" }}
                             label="I accept the General Condition and Policy"
                         />
-                    </div>
+                    </div> */}
 
 
                     {/* <Button
