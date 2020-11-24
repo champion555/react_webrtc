2e149e43330df30c016ee00a4678be7d4c63834c
softwarecomet <leapord198414@outlook.com>
2020-11-25 06:01:06 +0800

    update

--- src/Views/IDDocumentCamera/IDDocumentCamera.js
+++ src/Views/IDDocumentCamera/IDDocumentCamera.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { withRouter } from "react-router";
 import backURL from "../../assets/ic_back.png"
+import captureURL from "../../assets/camera_take.png"
+import errorURL from "../../assets/ic_error.png"
 import Webcam from "react-webcam";
 import Camera from 'react-dom-camera';
 import './IDDocumentCamera.css'
@@ -11,6 +13,9 @@ class IDDocumentCamera extends Component {
         super(props);
         this.state = {
             backButtonSRC: backURL,
+            captureButtonSRC:captureURL,
+            errorSRC:errorURL,
+
 
         }
     }
@@ -28,7 +33,7 @@ class IDDocumentCamera extends Component {
             facingMode: "environment"
         };
         return (
-            <div style={{ width: "100%", height:"100vh", background: "#7f00ff"}}>
+            <div style={{ width: "100%", height: "100vh", background: "#7f00ff" }}>
                 <div style={{ width: "100%", height: window.innerHeight * 0.07, alignItems: "center", display: "flex" }}>
                     <img src={this.state.backButtonSRC} style={{ width: "20px", height: "20px", marginLeft: "10px" }}
                         onClick={() => {
@@ -40,23 +45,37 @@ class IDDocumentCamera extends Component {
                 <div className="IDCamera-Container" style={{ height: window.innerHeight * 0.4 }}>
                     <Webcam
                         audio={false}
-                        height={window.innerHeight*0.4}
+                        height={window.innerHeight * 0.4}
                         mirrored={true}
-                        mirrored = {false}
+                        mirrored={false}
                         ref={this.setRef}
                         screenshotFormat="image/jpeg"
-                        imageSmoothing = {true}
+                        imageSmoothing={true}
                         width={"100%"}
-                        screenshotQuality = {1.0}
+                        screenshotQuality={1.0}
                         videoConstraints={videoConstraints}
                         forceScreenshotSourceSize="false"
                     />
                 </div>
                 <div className="IDMessage-Container" style={{ height: window.innerHeight * 0.2 }}>
-                    <p style={{ textAlign: "center", color: "white", fontSize: "20px", fontWeight: "bold", marginBottom: "0px" }}>this is id title</p>
-                    <p style={{ color: "white", }} className="message">this is message</p>
+                    <p className="IDTitle" >this is id title</p>
+                    <p className="IDDocCamMeassage">Place the front of National ID Card inside the frame and take the photo</p>
                 </div>
-                <div className="IDCapture-Container" style={{ height: window.innerHeight * 0.1 }}></div>
+                <div className = "errorMessageView" style={{ bottom: window.innerHeight * 0.13 }}>
+                    <div className = "container">
+                        <div className = "errortitle">   
+                            <img src={this.state.errorSRC} />
+                            <p>The image quality is very low</p>
+                        </div>
+                        <div className = "errormessage">
+                            <p>- Make sure the image is not blurry or contains blares!</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="IDCapture-Container">
+                    <img className = "IDCaptureButton" src={this.state.captureButtonSRC} />
+                    <p>powerd by BIOMIID</p>
+                </div>
             </div>
         )
     }
