831cb40b06de8cc50de601f86276609cdb9ad755
softwarecomet <leapord198414@outlook.com>
2020-11-25 22:56:31 +0800

    update

--- src/Views/POADocumentCamera/POADocumentCamera.js
+++ src/Views/POADocumentCamera/POADocumentCamera.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
 import { withRouter } from "react-router";
 import Webcam from "react-webcam";
+import Button from "../../Components/button/button"
 import Header from "../../Components/header/header"
+import errorURL from "../../assets/ic_error.png"
 import './POADocumentCamera.css'
 
 class POADocumentCamera extends Component {
-    constructor(props) {
+    constructor(props) {        
         super(props);
+        this.onCapture = this.onCapture.bind(this)
         this.state = {
             screenshot: null,
+            errorIconURL: errorURL,
+            previewImageStatuse: false,
+            isErrorStatus: false,
         }
     }
     componentDidMount = () => {
@@ -17,8 +23,14 @@ class POADocumentCamera extends Component {
     onCapture = () => {
         const imageSrc = this.webcam.getScreenshot();
         this.setState({ screenshot: imageSrc })
+        // console.log(imageSrc)
         console.log(this.state.screenshot)
+        this.setState({ previewImageStatuse: true })
     };
+    onReTake = () => {
+        this.setState({ previewImageStatuse: false })
+        this.setState({ isErrorStatus: false })
+    }
     setRef = webcam => {
         this.webcam = webcam;
     };
@@ -29,20 +41,55 @@ class POADocumentCamera extends Component {
         return (
             <div style={{ width: "100%", height: window.innerHeight }}>
                 <Header headerText="Scan PoA Document" />
-                <div className="POADocumentCamera-Container" style = {{height:window.innerHeight - 50}}>
-                    <Webcam
+                <div className="POADocumentCamera-Container" style={{ height: window.innerHeight - 50 }}>
+                {(!this.state.previewImageStatuse) && <Webcam
                         audio={false}
                         mirrored={true}
                         mirrored={false}
                         ref={this.setRef}
                         screenshotFormat="image/jpeg"
                         imageSmoothing={true}
-                        width={"100%"}
-                        height = {window.innerHeight*0.9}
+                        width={"90%"}
+                        height={window.innerHeight - 50}
                         screenshotQuality={1.0}
                         videoConstraints={videoConstraints}
-                        forceScreenshotSourceSize="flase" />
+                        forceScreenshotSourceSize="flase" />}
+                        {(this.state.previewImageStatuse) && <img className="PreviewImage" src={this.state.screenshot} style={{ height: window.innerHeight - 50}} />}
                 </div>
+                <div style={{ width: "100%", height: window.innerHeight - 50, position: "absolute", zIndex: "2" }}>
+                    {(this.state.isErrorStatus) && <div className="POAErrorMessageView" style={{ marginTop: window.innerHeight * 0.5 }}>
+                        <div className="container">
+                            <div className="errortitle">
+                                <img src={this.state.errorIconURL} />
+                                <p>The image quality is very low</p>
+                            </div>
+                            <div className="errormessage">
+                                <p>- Make sure the image is not blurry or contains blares!</p>
+                            </div>
+                        </div>
+                    </div>}
+                    {(!this.state.previewImageStatuse) && <div className = "POACapture-Container">
+                        <Button
+                            label="POA Document Scan"
+                            onClick={this.onCapture}
+                        />
+                        <p className="bottomTitle">powerd by BIOMIID</p>
+                    </div>}
+                    {(this.state.previewImageStatuse) && <div className = "POAPreviewButton-Container">
+                        {(!this.state.isErrorStatus) && <Button
+                            label="My photo is clear"
+                            onClick={() => {
+                                window.POADocPath = this.state.ImageURL
+                                this.props.history.push('photoliveness')
+                            }}
+                        />}
+                        <Button
+                            label="Re-take"
+                            onClick={this.onReTake}
+                        />
+                    </div>}
+                </div>
+
 
             </div>
         )
