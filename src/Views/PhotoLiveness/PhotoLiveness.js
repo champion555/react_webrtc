db37f6a954de364c649a199d48f83e68d4881c20
softwarecomet <leapord198414@outlook.com>
2020-11-26 03:31:59 +0800

    update

--- src/Views/PhotoLiveness/PhotoLiveness.js
+++ src/Views/PhotoLiveness/PhotoLiveness.js
@@ -85,7 +85,7 @@ class PhotoLiveness extends Component {
                         <h2 className="txtTitle">Face Liveness</h2>
                         <div style={{ width: '10px' }}></div>
                     </div>
-                    <div style={{ width: "100%", height: window.innerHeight * 0.68,backgroundImage:`url(${this.state.ImgSrc})`,backgroundSize:"100% 100%" }}></div>
+                    <div style={{ width: "100%", height: window.innerHeight * 0.681,backgroundImage:`url(${this.state.ImgSrc})`,backgroundSize:"100% 100%" }}></div>
                     <div className="liveness-captureButton" style={{ height: window.innerHeight * 0.25 }}>
                         <p style={{ font: "18px", color: "white", textAlign: "center", marginBottom: "5px" }}>Please place your face on the oval and take the photo </p>
                         <Button
