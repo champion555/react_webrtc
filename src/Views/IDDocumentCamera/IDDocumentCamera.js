26f8bb52d86094ce7ffba6b6e0d3b74238c52856
softwarecomet <leapord198414@outlook.com>
2020-11-25 16:58:24 +0800

    update

--- src/Views/IDDocumentCamera/IDDocumentCamera.js
+++ src/Views/IDDocumentCamera/IDDocumentCamera.js
@@ -134,7 +134,7 @@ class IDDocumentCamera extends Component {
                     />}
                     {(this.state.previewImageStatuse) && <img className="PreviewImage" src={this.state.screenshot} style={{ height:this.state.screenshot.innerHeight}} />}
                 </div>
-                <div style={{ zIndex: "2", position: "absolute", width: "100%", height: "100vh" }}>
+                <div style={{ zIndex: "2", position: "absolute", width: "100%", height: window.innerHeight }}>
                     <div style={{ height: window.innerHeight * 0.07, background: "#7f00ff" }}>
                         <div style={{ width: "100%", height: window.innerHeight * 0.07, alignItems: "center", display: "flex", background: "" }}>
                             <img src={this.state.backButtonSRC} style={{ width: "20px", height: "20px", marginLeft: "10px" }}
